817ba85c799347c4e7f1e4ef747b0f54ef2d4fe2
Cole Gleason <colewashere@gmail.com>
2014-01-24 01:46:55 -0600

    Added sync script and gitignore

---
+++ sync.sh
@@ -0,0 +1,32 @@
+if [ -r .config ]; then
+    echo "Reading settings from config file."
+    source .config
+    changed=true
+fi
+if [ -z "$remote_host" ]; then
+    default="web.engr.illinois.edu"
+    read -e -p "Please enter the hostname: [$default] " remote_host
+    remote_host="${remote_host:-$default}"
+fi
+if [ -z "$remote_user" ]; then
+    default=`whoami`
+    read -e -p "Please enter your username: [$default] " remote_user
+    remote_user="${remote_user:-$default}"
+fi
+if [ -z "$remote_dest" ]; then
+    default="~/public_html/"
+    read -e -p "Please enter the destination directory: [$default] " remote_dest
+    remote_dest="${remote_dest:-$default}"
+fi
+
+if [ -z "$changed" ]; then
+    read -p "Would you like to overwrite the config file with these values? [y/N] " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "remote_host=$remote_host\nremote_user=$remote_user\nremote_dest=\"$remote_dest\"\n" > ".config"
+    fi
+fi
+echo "Copying files to $remote_host with username $remote_user at directory $remote_dest."
+rsync -rav -e ssh --exclude='*.md' --exclude='*.sh' --exclude='.*' \
+. $remote_user@$remote_host:$remote_dest
